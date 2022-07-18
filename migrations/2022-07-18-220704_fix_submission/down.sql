5e81885e860c680cbe70197a88532e3eb8550f28
cassidoxa <cassidymoen@gmx.com>
2022-07-18 19:23:19 -0400

    Use proper migration for adding cascade delete on submissions table

---
+++ migrations/2022-07-18-220704_fix_submission/down.sql
@@ -0,0 +1,10 @@
+SET @SubmissionConstraint = (SELECT CONSTRAINT_NAME
+    FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+    WHERE TABLE_NAME='submissions' AND COLUMN_NAME='race_id');
+
+SET @DelStmt = CONCAT('ALTER TABLE submissions DROP FOREIGN KEY ', @SubmissionConstraint);
+PREPARE stmt FROM @DelStmt;
+EXECUTE stmt;
+
+ALTER TABLE submissions ADD FOREIGN KEY (race_id)
+    REFERENCES async_races(race_id);
